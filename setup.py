7f1b9b90d432f8e1d9c07389f89689de7b42fd5d
Daniel Sandborn <49823617+d-sandborn@users.noreply.github.com>
2025-11-17 11:59:31 -0800

    Update setup.py
    Change to match pypi package name.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-setup(name='pyTRACE',
+setup(name='trace',
       python_requires='>=3.10',
       version='0.1.0',
       description='Tracer-based Rapid Anthropogenic Carbon Estimation (TRACE) in Python',
