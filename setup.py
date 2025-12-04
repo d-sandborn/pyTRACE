a68d3f10c68e3c50913892ad7abc0d04434c9265
Daniel Sandborn <49823617+d-sandborn@users.noreply.github.com>
2025-12-04 12:42:42 -0800

    Update setup.py
    Prep for new version

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@
 from setuptools import setup, find_packages
 
 setup(name='trace',
-      python_requires='>=3.10',
-      version='0.1.0',
+      python_requires='>=3.12',
+      version='1.0.0',
       description='Tracer-based Rapid Anthropogenic Carbon Estimation (TRACE) in Python',
       long_description='After [TRACEv1](https://github.com/BRCScienceProducts/TRACEv1) and being developed in parallel with [ESPER](https://github.com/BRCScienceProducts/ESPER) and [PyESPER](https://github.com/LarissaMDias/PyESPER). Please reference the TRACEv1 [publication](https://doi.org/10.5194/essd-17-3073-2025) for further details. This work is the subject of a manuscript in preparation or review, and should be considered preliminary. This repository will be updated with any preprints and final published paper, and a new release will be produced pending publication. This routine generates estimates of ocean anthropogenic carbon content from user-supplied inputs of coordinates (lon, lat, depth), salinity, temperature, and year. Information is also needed about the historical and/or future atmospheric CO<sub>2</sub> trajectory.  This information can be provided or default values can be assumed.  This tool is a multi-platform implementation of the inverse gaussian transit time distribution method aimed at increasing the accessibility of ocean anthropogenic carbon content estimation.',
       author='Daniel Sandborn & Brendan Carter',
@@ -23,14 +23,14 @@ setup(name='trace',
           'tqdm',
           'seawater', #deprecated, kept for comparability with TRACEv1
           'gsw', #replacement for seawater
-          'PyCO2SYS @ git+https://github.com/mvdh7/PyCO2SYS@v2.0.0-b4',
+          'PyCO2SYS @ git+https://github.com/mvdh7/PyCO2SYS@v2.0.0-b4', #to be replaced with production version
           'numba',
           'shapely',
           'geopandas'
       ],
       classifiers=[
-          'Programming Language :: Python :: 3.12',
-          'Development Status :: 4 - Beta',
+          'Programming Language :: Python :: 3.13',
+          'Development Status :: 5 - Production/Stable',
           'Intended Audience :: Science/Research',
           'License :: OSI Approved :: MIT License',
           'Operating System :: OS Independent',
